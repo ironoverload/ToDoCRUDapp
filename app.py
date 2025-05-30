48027a8c94295d7845517d5c2284b8c03a3b763e
ironoverload <pottersj2005@gmail.com>
2025-05-30 12:00:44 +0900

    added more functions for update

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 
+# Import DB Functions from db_fxns
+#from db_fxns import *
+from db_fxns import create_table,add_data
+
+# Utility package for DateTime
+import datetime
+
 # EDA Packages
 import pandas as pd
 
 # Import the Functions page db_fxns.app
 from db_fxns import *
-
 
 # Data Viz Pkgs
 import plotly.express as px
@@ -23,9 +29,100 @@ HTML_BANNER = """
     </div>
     """
 
-
 def main():
     stc.html(HTML_BANNER)
+
+    menu = ["Create","Read","Update","Delete","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    # Create the database Tables
+    create_table()
+
+    # CREATE TASK
+    if choice == "Create":
+        st.subheader("Create New Task")
+
+        # Layout
+        col1, col2 = st.columns(2)
+
+        with col1:
+            task = st.text_area("Task To Do")
+
+        with col2:
+            task_status = st.selectbox("Status", ["To Do", "Ongoing", "Done"])
+            task_due_date = st.date_input("Task Due Date", datetime.date.today())
+
+        if st.button("Add Task"):
+            add_data(task, task_status, task_due_date)
+            st.success("Task Added:{}".format(task))
+
+    # READ/VIEW TASK
+    elif choice == "Read":
+        st.subheader("View Tasks")
+        result = view_all_data()
+        #st.write(result)
+        with st.expander("View All Tasks"):
+            df = pd.DataFrame(result,columns=["Task Name","Task Status","Task Due Date"])
+            st.dataframe(df)
+
+        # Add a plot
+        with st.expander("Task Status"):
+            task_df = df["Task Status"].value_counts().reset_index()
+            task_df.columns = ["Task Name", "Count"]
+            st.dataframe(task_df)
+
+            p1 = px.pie(task_df, names="Task Name", values="Count", title="Task Distribution")
+            st.plotly_chart(p1)
+
+    # UPDATE TASK
+    elif choice == "Update":
+        st.subheader("Edit/Update Task")
+        result = view_all_data()
+        # st.write(result)
+        with st.expander("View Current Tasks"):
+            df = pd.DataFrame(result, columns=["Task Name", "Task Status", "Task Due Date"])
+            st.dataframe(df)
+
+        #st.write(view_unique_tasks())
+        list_of_task = [i[0] for i in view_unique_tasks()]
+        #st.write(list_of_task)
+        # Creates a listbox with the unique list of tasks to edit
+        selected_task = st.selectbox("Select Task to Edit", list_of_task)
+
+        selected_result = get_task(selected_task)
+        st.write(selected_result)
+
+        if selected_result:
+            task = selected_result[0]
+            task_status = selected_result[1]
+            task_due_date = selected_result[2]
+
+            # Layout
+            col1, col2 = st.columns(2)
+
+            with col1:
+                new_task = st.text_area("Task To Do",task)
+
+            with col2:
+                new_task_status = st.selectbox("Status", ["To Do", "Ongoing", "Done"],index=["To Do", "Ongoing", "Done"].index(task_status))
+                new_task_due_date = st.date_input("Due Date",datetime.datetime.strptime(task_due_date, "%Y-%m-%d").date())
+
+
+            if st.button("Add Task"):
+                add_data(task, task_status, task_due_date)
+                st.success("Task Added:{}".format(task))
+
+
+
+
+    # DELETE TASK
+    elif choice == "Delete":
+        st.subheader("Delete Task")
+
+    else:
+        st.subheader("About")
+
+
 
 
 if __name__ == '__main__':
--- db_fxns.py
+++ db_fxns.py
@@ -1,3 +1,47 @@
+import streamlit as st
+import os
 import sqlite3
-conn = sqlite3.connect('data.db',check_same_thread=False)
+
+# Read DB path from secrets
+DB_NAME = st.secrets['database']["sqlite_path"]
+
+# Ensure the directory for the DB exists
+os.makedirs(os.path.dirname(DB_NAME), exist_ok=True)
+
+# The database Connection
+conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+
+#conn = sqlite3.connect('data/data.db',check_same_thread=False)
 c = conn.cursor()
+
+# Database
+# Table
+# Field/Columns
+# DataType
+
+# CREATE the database Table and columns
+def create_table():
+    c.execute("""CREATE TABLE IF NOT EXISTS taskstable(task TEXT, task_status TEXT, task_due_date DATE)""")
+
+# CREATE/INSERT New Tasks into the database table
+def add_data(task,task_status,task_due_date):
+    c.execute("""INSERT INTO taskstable(task, task_status, task_due_date) VALUES(?,?,?)""",(task,task_status,task_due_date))
+    conn.commit()
+
+# READ/VIEW All Tasks in the database table
+def view_all_data():
+    c.execute("""SELECT * FROM taskstable""")
+    return c.fetchall()
+
+# UPDATE Tasks
+# get unique tasks
+def view_unique_tasks():
+    c.execute("""SELECT DISTINCT task FROM taskstable""")
+    return c.fetchall()
+
+# get task by name
+def get_task(task):
+    c.execute("""SELECT * FROM taskstable WHERE task = ?""", (task,))
+    return c.fetchone()
+
+# DELETE Tasks
